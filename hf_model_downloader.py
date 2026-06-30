e7c813f6f348bb0fbf239c0e10f77adcd4f4376b
Jakapong <Jakapong.to01@gmail.com>
2026-06-30 16:01:22 +0700

    fix path defind in hf_model_downloader.py

--- hf_model_downloader.py
+++ hf_model_downloader.py
@@ -15,12 +15,12 @@ save_dir = "/home/ai-model/LLM/model"
 save_dir = Path(save_dir,Path(re.sub(r"\.", "_", model_repo)).stem)
 
 print(f"Downloading '{model_repo}' -> '{save_dir}'")
-# os.makedirs(save_dir, exist_ok=True)
+os.makedirs(save_dir, exist_ok=True)
 
-# local_path = snapshot_download(
-#     repo_id=model_repo,
-#     local_dir=save_dir,
-#     ignore_patterns=["*.msgpack", "*.h5", "flax_model*", "tf_model*"],
-# )
+local_path = snapshot_download(
+    repo_id=model_repo,
+    local_dir=save_dir,
+    ignore_patterns=["*.msgpack", "*.h5", "flax_model*", "tf_model*"],
+)
 
-# print(f"Download complete: {local_path}")
+print(f"Download complete: {local_path}")
